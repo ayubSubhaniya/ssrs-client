b30303bb627a08c317483b015b7404db4d08a3b2
sagar_savaliya <sagarsavaliya407@gmail.com>
2018-08-08 14:50:47 +0530

    background script added

--- src/components/App.js
+++ src/components/App.js
@@ -24,6 +24,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <canvas id="canvas"></canvas>
         <div class="loginbox">
         <Image src={logo}/>
         {
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import Background from './js/background';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+Background();
+
 registerServiceWorker();
