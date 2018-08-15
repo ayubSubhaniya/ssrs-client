88eb76f18926b546ebb417faa6f8996149d0856d
sagar-savaliya <sagarsavaliya407@gmail.com>
2018-08-15 22:48:38 +0530

    button added for verification link

--- src/components/public-page/Signup.js
+++ src/components/public-page/Signup.js
@@ -11,40 +11,25 @@ class SignUp extends Component {
             password: '',
             confirmPassword: '',
             isSignup: false,
-            //verificationLink: ''
         }
     }
 
-    handleDaiictIdChange = (event) => {
+    handleChange = ({target}) => {
         this.setState({
-            daiictId: event.target.value
-        })
-    }
-
-    handlePasswordChange = (event) => {
-        this.setState({
-            password: event.target.value
-        })
-    }
-
-    handleConfirmPasswordChange = (event) => {
-        this.setState({
-            confirmPassword: event.target.value
+            [target.name]: target.value
         })
     }
 
     handleSignUp = () => {
         if (this.state.password != this.state.confirmPassword) {
-            alert("Password not same");
+            alert("Password and Confirmed Password are not same");
             return;
         }
-        //delete this.state.confirmPassword;
-        
 
-        var url = domainUrl + '/account/signup'
+        var url = domainUrl + '/account/signup';
         var userObj = {
-          daiictId: this.state.daiictId,
-          password: this.state.password
+            daiictId: this.state.daiictId,
+            password: this.state.password
         };
         var request = new XMLHttpRequest();
         request.open('POST', url, true);
@@ -54,10 +39,6 @@ class SignUp extends Component {
         request.onload = function () {
             if (this.status == HttpStatus.CREATED) {
                 that.setState({isSignup: true});
-                /*var response = JSON.parse(request.responseText);
-                that.setState({verificationLink:response.resendVerificationLink})
-                console.log(that.state.verificationLink);*/
-                // window.open("\homepage_admin.html", "_self");
             }
             ;
         };
@@ -65,36 +46,34 @@ class SignUp extends Component {
     }
 
     handleResendVerificationLink = () => {
-      var url = domainUrl + '/account/resendVerificationLink/' + this.state.daiictId;
-      var request = new XMLHttpRequest();
-      request.open('GET', url, true);
-      request.withCredentials =  true;
-      request.send();
+        var url = domainUrl + '/account/resendVerificationLink/' + this.state.daiictId;
+        var request = new XMLHttpRequest();
+        request.open('GET', url, true);
+        request.withCredentials = true;
+        request.send();
     }
 
 
     render() {
         return (
-            <form>
-                <p>DA-IICT ID</p>
-                <input type="text" name="" placeholder="Enter Student ID" id="username"
-                       value={this.state.daiictId} onChange={this.handleDaiictIdChange}
-                />
-                <p>Password</p>
-                <input type="password" name="" placeholder="Enter Password" id="passwd"
-                       value={this.state.password} onChange={this.handlePasswordChange}
-                />
-                <p>Confirm Password</p>
-                <input type="password" name="" placeholder="Confirm Password" id="passwd"
-                       value={this.state.confirmPassword} onChange={this.handleConfirmPasswordChange}
-                />
-                <input type="button" name="" value="SignUp" id="log" onClick={this.handleSignUp}/><br/>
-                {
-                  this.state.isSignup
-                  ?<input type="button" name="" value="Resend confirmation link" id="resendVerificationLink" onClick={this.handleResendVerificationLink}/>
-                  :''
-                }
-            </form>
+            this.state.isSignup
+                ? (<input type="button" value="Resend confirmation link"
+                          onClick={this.handleResendVerificationLink}/>)
+                : (<form>
+                    <p>DA-IICT ID</p>
+                    <input type="text" name={'daiictId'} placeholder="Enter Student ID"
+                           value={this.state.daiictId} onChange={this.handleChange}
+                    />
+                    <p>Password</p>
+                    <input type="password" name={'password'} placeholder="Enter Password"
+                           value={this.state.password} onChange={this.handleChange}
+                    />
+                    <p>Confirm Password</p>
+                    <input type="password" name={'confirmPassword'} placeholder="Confirm Password"
+                           value={this.state.confirmPassword} onChange={this.handleChange}
+                    />
+                    <input type="button" value="SignUp" onClick={this.handleSignUp}/><br/>
+                </form>)
         );
     }
 }
