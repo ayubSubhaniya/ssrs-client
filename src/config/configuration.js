41ba8a1bea407775b611e13d6b3ea8287e86172a
aya-cool <ayub.subhaniya@gmail.com>
2018-10-07 16:59:30 +0530

    added error messages

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -2,12 +2,14 @@ export const domainUrl = "https://ssrs-daiict-api.herokuapp.com";
 //export const domainUrl = "http://localhost:3001";
 export const errorMessages = {
     internalServerError: "Internal server error. Please try again later!",
-    serverTimeout: "Server timeout. Please try again later",
+    serverTimeout: "Server timeout. Please try again later!",
     userAlreadyExist: "User Already Exist!",
     userNotExist: "User don't Exist!",
     incorrectUserNameOrPassword: "Incorrect username/password",
     somethingsWrong: "Something's wrong. Please try again later!",
-    forbidden: "You don't have access to requested resource",
+    forbidden: "You don't have access to requested resource!",
+    notFound: "Not found!",
+    noCollectionTypes: "No collection type information provided. Please select collection type and provide information."
 };
 
 export const infoMessages = {
