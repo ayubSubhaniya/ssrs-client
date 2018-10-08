13b51a72fd7781e4d5269722c8e01cd5d952a3f1
Harsh Vasoya <34365323+harshvasoya008@users.noreply.github.com>
2018-10-08 16:23:17 +0530

    signin, signup onPress Enter

--- src/components/public-page/SignInPage.js
+++ src/components/public-page/SignInPage.js
@@ -2,25 +2,33 @@ import TextInputUserName from "./TextInputUserName";
 import React from "react";
 import ErrorMessage from "../error/ErrorMessage";
 
-export default function SignInPage({daiictId, handleChange, password, loginMessage, logIn, openModal, clearLoginMessage}) {
+export default function SignInPage({ daiictId, handleChange, password, loginMessage, logIn, openModal, clearLoginMessage }) {
     return (
         <div className="page">
-            <TextInputUserName daiictId={daiictId}
-                               handleChange={handleChange}/>
-            <div className="page-input">
-                <div className="title"><i className="fa fa-lock"></i> PASSWORD
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                logIn({
+                    daiictId: daiictId,
+                    password: password
+                })
+            }}>
+                <TextInputUserName daiictId={daiictId}
+                    handleChange={handleChange} />
+                <div className="page-input">
+                    <div className="title"><i className="fa fa-lock"></i> PASSWORD
+                    </div>
+                    <input className="form-control" type="password" name="password" value={password}
+                        onChange={handleChange} />
+                    <ErrorMessage message={loginMessage} clearMessage={clearLoginMessage} />
                 </div>
-                <input className="form-control" type="password" name="password" value={password}
-                       onChange={handleChange}/>
-                <ErrorMessage message={loginMessage} clearMessage={clearLoginMessage}/>
-            </div>
-            <div className="page-input"><input type="submit" value="ENTER"
-                                               onClick={() => logIn({
-                                                   daiictId: daiictId,
-                                                   password: password
-                                               })}/></div>
-            <input type="button" className={'page-link-cstm'} value="Forgot Password"
-                   onClick={openModal}/>
+                <div className="page-input"><input type="submit" value="ENTER"
+                    onClick={() => logIn({
+                        daiictId: daiictId,
+                        password: password
+                    })} /></div>
+                <input type="button" className={'page-link-cstm'} value="Forgot Password"
+                    onClick={openModal} />
+            </form>
         </div>
     )
 }
