2adfba4e72584d663fa4ce866371d49dd57edbe1
Harsh Vasoya <34365323+harshvasoya008@users.noreply.github.com>
2018-10-23 13:13:59 +0530

    [changed] header spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class Header extends Component {
     render() {
         return (
-            <h2 className='header'>{this.props.title}</h2>
+            <h2 className='header mb-3 mt-3'>{this.props.title}</h2>
         );
     }
 }
