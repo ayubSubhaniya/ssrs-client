a76ca19c978a5921e75706ef6ddea3d21aa962c8
Tapan Modi <tapanr97@gmail.com>
2018-11-08 17:34:35 +0530

    about us

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import NavigationBar from '../NavigationBar';
-import { SocialIcon } from 'react-social-icons';
 
 class AboutUs extends Component{
     constructor(props, context) {
@@ -8,10 +7,10 @@ class AboutUs extends Component{
     }
 
     render() {
+
         return(
             <div>
                 <NavigationBar/>
-                <div>    
                     <h1 class="my-header deepshadow" style={{textAlign:"center"}}>Meet Our Team<br/>                    
                     <h2 class="my-header2"><span style={{color:"red"}}>"</span>None Of Us Is As Smart As All Of Us<span style={{color:"red"}}>"</span></h2></h1>
 
@@ -27,10 +26,11 @@ class AboutUs extends Component{
                             <p>Competitive Programmer</p>
                             <p>Intern & FTE At Sprinklr</p>
                             <hr class="style14"></hr>
-                            <SocialIcon url="https://www.facebook.com/sagar.savaliya407" 
-                                        style={{height:"40px", width:"40px", marginRight:"2%", marginBottom:"40px"}}/>
-                            <SocialIcon url="https://www.linkedin.com/in/sagar-savaliya-0825a3128/" 
-                                        style={{height:"40px", width:"40px",marginBottom:"40px"}}/>
+                            <div class="wrapper">
+                                <a target="_blank" href="https://www.facebook.com/sagar.savaliya407"><i class="fa fa-3x fa-facebook-square"></i></a>
+                                <a target="_blank" href="https://github.com/sagar-savaliya"><i class="fa fa-3x fa-github"></i></a>                        
+                                <a target="_blank" href="https://www.linkedin.com/in/sagar-savaliya-0825a3128/"><i class="fa fa-3x fa-linkedin-square"></i></a>
+                            </div>
                         </div>
                         <div class="col-lg-4 col-sm-6 text-center mb-4">
                             <img class="rounded-circle img-fluid d-block mx-auto" src={require("./Ayub.jpg")} alt=""
@@ -42,10 +42,13 @@ class AboutUs extends Component{
                             <p>Competitive Programmer</p>
                             <p>Intern & FTE At Sprinklr</p>
                             <hr class="style14"></hr>
-                            <SocialIcon url="https://www.facebook.com/ayub.subhaniya" 
-                                        style={{height:"40px", width:"40px", marginRight:"2%", marginBottom:"40px"}}/>
-                            <SocialIcon url="https://www.linkedin.com/in/ayub-subhaniya-7632a4112/" 
-                                        style={{height:"40px", width:"40px",marginBottom:"40px"}}/>
+
+                            <div class="wrapper">
+                                <a target="_blank" href="https://www.facebook.com/ayub.subhaniya"><i class="fa fa-3x fa-facebook-square"></i></a>
+                                <a target="_blank" href="https://github.com/ayubSubhaniya"><i class="fa fa-3x fa-github"></i></a>                        
+                                <a target="_blank" href="https://www.linkedin.com/in/ayub-subhaniya-7632a4112/"><i class="fa fa-3x fa-linkedin-square"></i></a>
+                                <a target="_blank" href="https://plus.google.com/101960984824622972479"><i class="fa fa-3x fa-google-plus"></i></a>
+                            </div>
                         </div>
                         <div class="col-lg-4 col-sm-6 text-center mb-4">
                             <img class="rounded-circle img-fluid d-block mx-auto" src={require("./Tapan.jpg")} alt=""
@@ -56,44 +59,23 @@ class AboutUs extends Component{
                             <p>Front-End Developer</p>
                             <p>Competitive Programmer</p>
                             <p>Intern & FTE At Goldman Sachs</p>
-                            <hr class="style14"></hr>
-                            <SocialIcon url="https://www.facebook.com/tapanr97" 
-                                        style={{height:"40px", width:"40px", marginRight:"2%", marginBottom:"40px"}}/> 
-                            <SocialIcon url="https://plus.google.com/+TapanModi" 
-                                        style={{height:"40px", width:"40px", marginRight:"2%",marginBottom:"40px"}}/> 
-                            <SocialIcon url="https://www.linkedin.com/in/tapan-modi-aa420571/" 
-                                        style={{height:"40px", width:"40px",marginBottom:"40px"}}/> 
+                            <hr class="style14"></hr> 
+                            {/* <ul class="social-icons" style={{marginLeft:"0%"}}>
+                                <li style={{marginRight:"2%"}}><a href="https://www.facebook.com/tapanr97" target="_blank" >
+                                            <img src={require("./Social Media Icons/SVG/Circle Color/Facebook.svg")} /></a></li>
+                                <li style={{marginRight:"2%"}}><a href="https://plus.google.com/+TapanModi" target="_blank" >
+                                            <img src={require("./Social Media Icons/SVG/Circle Color/Google+.svg")} /></a></li>                                            
+                                <li><a href="https://www.linkedin.com/in/tapan-modi-aa420571/"  target="_blank">
+                                            <img src={require("./Social Media Icons/SVG/Circle Color/Linkedin.svg")} /></a></li>
+                            </ul>     */}
+                            <div class="wrapper">
+                                <a target="_blank" href="https://www.facebook.com/tapanr97"><i class="fa fa-3x fa-facebook-square"></i></a>
+                                <a target="_blank" href="https://github.com/tapanr97"><i class="fa fa-3x fa-github"></i></a>                        
+                                <a target="_blank" href="https://www.linkedin.com/in/tapan-modi-aa420571/"><i class="fa fa-3x fa-linkedin-square"></i></a>
+                                <a target="_blank" href="https://plus.google.com/+TapanModi"><i class="fa fa-3x fa-google-plus"></i></a>
+                            </div>                                    
                         </div>
-                        <div class="col-lg-4 col-sm-6 text-center mb-4">
-                            <img class="rounded-circle img-fluid d-block mx-auto" src={require("./Dhruv.jpg")} alt=""
-                                        style={{width:"200px", height:"200px"}}/>
-                            <h3 style={{color:"#e50202",marginTop:"2%"}}>Dhruv Patel
-                            </h3>
-                            <hr class="style14"></hr>
-                            <p>Front-End Developer</p>
-                            <p>Competitive Programmer</p>
-                            <p>FTE At Oracle</p>
-                            <hr class="style14"></hr>
-                            <SocialIcon url="https://www.facebook.com/profile.php?id=100009951722951" 
-                                        style={{height:"40px", width:"40px", marginRight:"2%", marginBottom:"40px"}}/>
-                            <SocialIcon url="https://www.linkedin.com/in/dhruv-patel-aa8522125/" 
-                                        style={{height:"40px", width:"40px",marginBottom:"40px"}}/> 
-                        </div>
-                        <div class="col-lg-4 col-sm-6 text-center mb-4">
-                            <img class="rounded-circle img-fluid d-block mx-auto" src={require("./Harsh.JPG")} alt=""
-                                        style={{width:"200px", height:"200px"}}/>
-                            <h3 style={{color:"#e50202",marginTop:"2%"}}>Harsh Vasoya
-                            </h3>
-                            <hr class="style14"></hr>
-                            <p>Back-End Developer</p>
-                            <p>Competitive Programmer</p>
-                            <p>Intern At Prop-Tiger</p>
-                            <hr class="style14"></hr>
-                            <SocialIcon url="https://www.facebook.com/harsh.vasoya.083" 
-                                        style={{height:"40px", width:"40px", marginRight:"2%", marginBottom:"40px"}}/>
-                            <SocialIcon url="https://www.linkedin.com/in/harsh-vasoya-463828160/" 
-                                        style={{height:"40px", width:"40px",marginBottom:"40px"}}/>
-                        </div>
+
                         <div class="col-lg-4 col-sm-6 text-center mb-4">
                             <img class="rounded-circle img-fluid d-block mx-auto" src={require("./Dhaval.jpg")} alt=""
                                         style={{width:"200px", height:"200px"}}/>
@@ -104,14 +86,47 @@ class AboutUs extends Component{
                             <p>Competitive Programmer</p>
                             <p>Intern At Sprinklr</p>
                             <p>FTE At Amadeus Labs</p>
+                            <hr class="style14"></hr> 
+                            <div class="wrapper">
+                                <a target="_blank" href="https://www.facebook.com/dhavaljlila"><i class="fa fa-3x fa-facebook-square"></i></a>
+                                <a target="_blank" href="https://github.com/DhavalLila"><i class="fa fa-3x fa-github"></i></a>                        
+                                <a target="_blank" href="https://www.linkedin.com/in/dhaval-lila-822895163/"><i class="fa fa-3x fa-linkedin-square"></i></a>
+                            </div>        
+                        </div>
+                        <div class="col-lg-4 col-sm-6 text-center mb-4">
+                            <img class="rounded-circle img-fluid d-block mx-auto" src={require("./Harsh.JPG")} alt=""
+                                        style={{width:"200px", height:"200px"}}/>
+                            <h3 style={{color:"#e50202",marginTop:"2%"}}>Harsh Vasoya
+                            </h3>
                             <hr class="style14"></hr>
-                            <SocialIcon url="https://www.facebook.com/dhavaljlila" 
-                                        style={{height:"40px", width:"40px", marginRight:"2%", marginBottom:"40px"}}/>
-                            <SocialIcon url="https://www.linkedin.com/in/dhaval-lila-822895163/" 
-                                        style={{height:"40px", width:"40px",marginBottom:"40px"}}/>
+                            <p>Back-End Developer</p>
+                            <p>Competitive Programmer</p>
+                            <p>Employee At Prop-Tiger</p>
+                            <hr class="style14"></hr>
+                            <div class="wrapper">
+                                <a target="_blank" href="https://www.facebook.com/harsh.vasoya.083"><i class="fa fa-3x fa-facebook-square"></i></a>
+                                <a target="_blank" href="https://github.com/harshvasoya008"><i class="fa fa-3x fa-github"></i></a>                        
+                                <a target="_blank" href="https://www.linkedin.com/in/harsh-vasoya-463828160/"><i class="fa fa-3x fa-linkedin-square"></i></a>
+                            </div>                                                                        
+                        </div>
+                        <div class="col-lg-4 col-sm-6 text-center mb-4">
+                            <img class="rounded-circle img-fluid d-block mx-auto" src={require("./Dhruv.jpg")} alt=""
+                                        style={{width:"200px", height:"200px"}}/>
+                            <h3 style={{color:"#e50202",marginTop:"2%"}}>Dhruv Patel
+                            </h3>
+                            <hr class="style14"></hr>
+                            <p>Front-End Developer</p>
+                            <p>Competitive Programmer</p>
+                            <p>FTE At Oracle</p>
+                            <hr class="style14"></hr>  
+                            <div class="wrapper">
+                                <a target="_blank" href="https://www.facebook.com/profile.php?id=100009951722951"><i class="fa fa-3x fa-facebook-square"></i></a>
+                                <a target="_blank" href="https://github.com/pdhruv1109"><i class="fa fa-3x fa-github"></i></a>                        
+                                <a target="_blank" href="https://www.linkedin.com/in/dhruv-patel-aa8522125/"><i class="fa fa-3x fa-linkedin-square"></i></a>
+                            </div>                                     
                         </div>
                     </div>
-                </div>
+
           </div>
         );
     }
