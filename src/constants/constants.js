d0326a85fd8715d1772d244d5e411fb51d45e497
sagar-savaliya <sagarsavaliya407@gmail.com>
2018-11-13 16:11:15 +0530

    cart and order status update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -24,7 +24,7 @@ export const defaultCart = {
     orders: [],
     ordersCost: "Loading...",
     requestedBy: "",
-    status: 500,
+    status: 0,
     totalCost: 0,
     _id: "Loading...",
     pickup: '',
